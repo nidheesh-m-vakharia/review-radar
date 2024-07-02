5c736239b5752d454f2c0a036f501e6e71bd09cc
nidheesh-m-vakharia <nvakh003@ucr.edu>
2024-07-02 00:26:03 -0700

    Correcting getStaticParams()

--- app/form/actions.js
+++ app/form/actions.js
@@ -1,9 +1,7 @@
-'use server';
+"use server";
 
-import { redirect} from 'next/navigation';
+import { redirect } from "next/navigation";
 
 export async function postToResult(formData) {
-  redirect(`/result/${formData.get('link')}`);
-
-
+  redirect(`/result/${formData.get("link")}`);
 }
--- app/result/[prodId]/page.js
+++ app/result/[prodId]/page.js
@@ -1,11 +1,13 @@
-'use client';
-export default function Page({params}) {
+export function generateStaticParams() {
+  return [{ prodId: "1" }, { prodId: "2" }, { prodID: "3" }];
+}
+
+export default function Page({ params }) {
   return (
     <div>
       <h1>Result</h1>
       <p>Link: {params.prodId}</p>
       {console.log(params)}
-
     </div>
   );
 }
