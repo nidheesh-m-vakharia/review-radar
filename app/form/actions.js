2827aa076802d10814b935e50136c66b56dffef8
Nidheesh Manishkumar Vakharia <149847732+nidheesh-m-vakharia@users.noreply.github.com>
2024-07-04 09:09:07 +0530

    Added product ID and Title detetction actions (#18)

--- app/form/actions.js
+++ app/form/actions.js
@@ -1,9 +1,15 @@
-'use server';
+"use server";
 
-import { redirect} from 'next/navigation';
+import { redirect } from "next/navigation";
 
+export async function getProductId(link) {
+  const url = new URL(link);
+  const path = url.pathname.split("/");
+
+  return path[3];
+}
 export async function postToResult(formData) {
-  redirect(`/result/${formData.get('link')}`);
-
-
+  await getProductId(formData.get("link")).then((productId) => {
+    redirect(`/result/${productId}`);
+  });
 }
--- app/result/[prodId]/page.js
+++ app/result/[prodId]/page.js
@@ -1,11 +1,29 @@
-'use client';
-export default function Page({params}) {
-  return (
-    <div>
-      <h1>Result</h1>
-      <p>Link: {params.prodId}</p>
-      {console.log(params)}
+"use client";
+import { getProductName } from "./actions.js";
+import { useState, useEffect } from "react";
 
+export default function Page({ params }) {
+  const [productName, setProductName] = useState("");
+  useEffect(() => {
+    getProductName(params.prodId).then((name) => {
+      setProductName(name);
+    });
+  }, []);
+
+  return (
+    <div className="flex flex-col  justify-center items-center h-[100vh]">
+      <div className="text-white bg-black text-center w-[18em] p-10 rounded-xl">
+        <h1 className="text-3xl font-bold mb-10">Result</h1>
+        <p className="mb-4">
+          Product ID:
+          <br /> {params.prodId}
+        </p>
+        <p>
+          Name:
+          <br />
+          {productName ? productName : "Loading..."}
+        </p>
+      </div>
     </div>
   );
 }
